7a952b2fdcf63444b6dd9c0ebed498ea5aedc408
Sergei <sergei@winback.com>
2025-05-13 17:03:17 +0200

    Fix TypeScript type errors for Render deployment

--- src/healthz.ts
+++ src/healthz.ts
@@ -1,8 +1,8 @@
 // Health check endpoint for Render
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 export const healthCheckRouter = express.Router();
 
-healthCheckRouter.get('/', (req, res) => {
+healthCheckRouter.get('/', (req: Request, res: Response) => {
   res.status(200).json({ status: 'ok', message: '2Serv API is running' });
 });
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,8 +1,8 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import { INTERNAL_SERVER_ERROR } from "../constants/http";
 import logger from "../utils/logger";
 
-const errorHandler: ErrorRequestHandler = (error, req, res, _) => {
+const errorHandler: ErrorRequestHandler = (error: any, req: Request, res: Response, _: NextFunction) => {
   logger.error(`PATH: ${req.path}\n${error}`);
   res.status(INTERNAL_SERVER_ERROR).send("Internal server error.");
 }
